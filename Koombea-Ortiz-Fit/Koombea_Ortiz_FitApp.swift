f62d7356aa1e26120e24e8ff78947c64f9790112
davit <david.ortiz.developer@gmail.com>
2021-02-12 18:31:43 -0500

    Initial Commit

---
+++ Koombea-Ortiz-Fit/Koombea_Ortiz_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koombea_Ortiz_FitApp.swift
+//  Koombea-Ortiz-Fit
+//
+//  Created by edgar david ortiz diaz on 12/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Koombea_Ortiz_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
